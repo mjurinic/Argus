fa8da5cc184e5ef01bac27555fef01e475d876e4
Mislav Jurinic <jurinic.mislav@gmail.com>
2017-10-31 02:04:13 +0100

    runtime permission check

--- app/src/main/java/com/mjurinic/argus/argus/activities/CameraActivity.java
+++ app/src/main/java/com/mjurinic/argus/argus/activities/CameraActivity.java
@@ -1,7 +1,12 @@
 package com.mjurinic.argus.argus.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 
@@ -13,11 +18,14 @@ import butterknife.ButterKnife;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+
     @BindView(R.id.camera_preview)
     FrameLayout cameraPreview;
 
     private Camera camera;
     private BasicSurfaceView surfaceView;
+    private boolean hasCameraPermission;
 
     private void initSurfaceView() {
         surfaceView = new BasicSurfaceView(getApplicationContext(), camera);
@@ -25,7 +33,7 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     private void initCamera() {
-        if (camera == null) {
+        if (hasPermissions() && camera == null) {
             camera = Camera.open();
             camera.setDisplayOrientation(90);
 
@@ -43,16 +51,33 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
+    public boolean hasPermissions() {
+        hasCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+
+        if (!hasCameraPermission) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+
+        return hasCameraPermission;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA: {
+                hasCameraPermission = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_camera);
         ButterKnife.bind(this);
-
-        // TODO permissions
-
-        initCamera();
     }
 
     @Override
